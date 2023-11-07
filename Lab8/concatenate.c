9a0181a6a623b1113efd49b54727b701a41b663f
Aditya Wadgaonkar <119002724+adi673@users.noreply.github.com>
2023-11-07 13:16:37 +0530

    Update concatenate.c

--- Lab8/concatenate.c
+++ Lab8/concatenate.c
@@ -1,1 +1,16 @@
 
+void concatenate(struct node *a,struct node *b)
+{
+    if( a != NULL && b!= NULL )
+    {
+        if (a->next == NULL)
+            a->next = b;
+            b->prev=a;
+        else
+            concatenate(a->next,b);
+    }
+    else
+    {
+        printf("Either a or b is NULL\n");
+    }
+}
